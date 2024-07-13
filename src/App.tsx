eeb44ba855fe153ef66c79736ea12891b6cca7f3
italloVidal <itallovidalll@gmail.com>
2024-07-13 03:58:43 -0300

    setting up tailwind and shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import './style/global.css'
+import {Button} from "@/components/ui/button.tsx";
+
 
 export function App() {
 
   return (
-    <>
-
-    </>
+    <div className={'flex h-screen justify-center items-center'}>
+      <Button>Enviar</Button>
+    </div>
   )
 }
 
