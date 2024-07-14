505c4ad8796d6b792b1e025d284004d2601bdb10
itallovidal <itallovidalll@gmail.com>
2024-07-14 19:56:28 -0300

    sign page style

--- src/pages/_layout/authLayout.tsx
+++ src/pages/_layout/authLayout.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { Pizza } from 'lucide-react'
 
 export function AuthLayout() {
   return (
-    <div>
-      <div>
+    <div className={'min-h-screen grid grid-cols-2'}>
+      <div
+        className={
+          'h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground flex flex-col justify-between'
+        }
+      >
+        <header className={'flex items-center gap-3 text-lg text-foreground'}>
+          <Pizza />
+          <span className={'font-semibold'}> pizza.shop</span>
+        </header>
+        <footer className={'text-sm'}>
+          Painel do parceiro &copy; pizza.shop - {new Date().getFullYear()}
+        </footer>
+      </div>
+      <div className={'flex flex-col items-center justify-center'}>
         <Outlet />
       </div>
     </div>
